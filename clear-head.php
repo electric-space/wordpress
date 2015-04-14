b441da135dbe0ba11dd591d0c11df5ac7c5d9d38
electric-space <admin@electricspace.co.uk>
2015-04-14 12:59:32 +0100

    Addin clear-head

---
+++ clear-head.php
@@ -0,0 +1,7 @@
+// Clean up the <head>
+function removeHeadLinks() {
+	remove_action('wp_head', 'rsd_link');
+	remove_action('wp_head', 'wlwmanifest_link');
+	remove_action('wp_head', 'wp_generator');
+}
+add_action('init', 'removeHeadLinks');
